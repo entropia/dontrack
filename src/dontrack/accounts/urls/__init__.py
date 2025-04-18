dd96f5a785cfca931343786e44d6b06297dd2d22
Adrian Nöthlich <git@promasu.tech>
2025-04-18 23:16:26 +0200

    fix(accounts): Add profile page for login

--- src/dontrack/accounts/urls/__init__.py
+++ src/dontrack/accounts/urls/__init__.py
@@ -2,13 +2,16 @@ from typing import Any, List
 
 from django.urls import path
 
-from dontrack.accounts.views.auth import AuthorizeSSOUser, UserLoginView, UserLogoutView
+from memon.accounts.views.auth import AuthorizeSSOUser, UserLoginView, UserLogoutView
+from memon.accounts.views.profile import UserProfileView
 
 
 urlpatterns: List[Any] = [
     # auth
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
+    # profile
+    path('profile/', UserProfileView.as_view(), name='user_profile'),
     # sso
     path('auth/', AuthorizeSSOUser.as_view(), name='auth'),
 ]
---
+++ src/dontrack/accounts/views/profile.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils.translation import gettext_lazy as _
+from django.views.generic import DetailView
+
+from memon.accounts.models import User
+
+
+class UserProfileView(LoginRequiredMixin, DetailView):
+    model = User
+    object: User
+    save_path_in_session = True
+    title = _('My account')
+
+    def get_object(self, queryset=None):
+        return self.request.user
