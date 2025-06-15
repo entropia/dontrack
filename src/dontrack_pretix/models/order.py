a77e2283a9f6fad9f62799c22a5a7020da86eed6
Adrian Nöthlich <git@promasu.tech>
2025-06-15 12:04:01 +0200

    fix: missing translation

--- src/dontrack_pretix/models/order.py
+++ src/dontrack_pretix/models/order.py
@@ -12,16 +12,16 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    order_id = models.CharField(max_length=255, null=False, blank=False, verbose_name=_('Pretix Order ID'))
-    position_id = models.IntegerField(null=False, blank=False, verbose_name=_('Pretix Position ID'))
+    order_id = models.CharField(max_length=255, null=False, blank=False, verbose_name=_('Pretix order id'))
+    position_id = models.IntegerField(null=False, blank=False, verbose_name=_('Pretix position id'))
 
     donation = models.ForeignKey('donations.Donation', null=False, on_delete=models.CASCADE, related_name="pretix_orders",
                               verbose_name=_("Donation"))
 
     class Meta:
         ordering = ["created_at"]
-        verbose_name = _("Pretix Order")
-        verbose_name_plural = _("Pretix Orders")
+        verbose_name = _("Pretix order")
+        verbose_name_plural = _("Pretix orders")
         default_permissions = ()
         unique_together = ("order_id", "position_id")
 
