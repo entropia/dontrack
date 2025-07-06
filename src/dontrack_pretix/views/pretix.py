db2ea0add60fe801cc8d58c542a204fd6765cb09
Adrian Nöthlich <git@promasu.tech>
2025-07-06 20:51:02 +0200

    fix(gpn23): Wrong generated qr-codes

--- src/dontrack_pretix/views/pretix.py
+++ src/dontrack_pretix/views/pretix.py
@@ -11,32 +11,38 @@ from dontrack_pretix.models.order import Order
 from dontrack_pretix.utils.pretix_api import PretixApiClient
 
 
+def process_order(organizer: str, event: str, pseudo_id: str) -> HttpResponse:
+    api_client = PretixApiClient(settings.PRETIX_ROOT_URL, organizer, event, settings.PRETIX_API_KEYS[organizer])
+
+    # Get eligible products
+    product_ids = api_client.get_products_in_quota(settings.PRETIX_DONATION_QUOTA)
+
+    if product_ids is None:
+        return HttpResponseNotFound()
+
+    # Retrieve order information
+    order = api_client.get_orderpositions({'pseudonymization_id': pseudo_id})
+
+    # There should only be one order returned. Expect tampering otherwise and abort
+    if len(order) == 1 and order[0].get('item') in product_ids:
+        # Check if order is already imported so redirect else import order
+        try:
+            registered_order = Order.objects.get(order_id=order[0].get('order'), position_id=order[0].get('positionid'))
+            return HttpResponseRedirect(reverse_lazy('donor_registration', kwargs={'pk': registered_order.donation.pk}))
+        except Order.DoesNotExist:
+            donation = Donation.objects.create(amount=order[0].get('price'))
+            Order.objects.create(order_id=order[0].get('order'), position_id=order[0].get('positionid'),
+                                 donation=donation)
+            return HttpResponseRedirect(reverse_lazy('donor_registration', kwargs={'pk': donation.pk}))
+    else:
+        if event == 'gpn23':
+            return process_order(organizer, 'gpn23merch', pseudo_id)
+        return HttpResponseNotAllowed(permitted_methods='GET')
+
 
 class PretixImportView(RedirectView):
     def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
-        api_client = PretixApiClient(settings.PRETIX_ROOT_URL, kwargs['organizer'], kwargs['event'], settings.PRETIX_API_KEYS[kwargs['organizer']])
-
-        # Get eligible products
-        product_ids = api_client.get_products_in_quota(settings.PRETIX_DONATION_QUOTA)
-
-        if product_ids is None:
-            return HttpResponseNotFound()
-
-        # Retrieve order information
-        order = api_client.get_orderpositions({'pseudonymization_id': kwargs['pseudo_id']})
-
-        # There should only be one order returned. Expect tampering otherwise and abort
-        if len(order) == 1 and order[0].get('item') in product_ids:
-            # Check if order is already imported so redirect else import order
-            try:
-                registered_order = Order.objects.get(order_id=order[0].get('order'), position_id=order[0].get('positionid'))
-                return HttpResponseRedirect(reverse_lazy('donor_registration', kwargs={'pk': registered_order.donation.pk}))
-            except Order.DoesNotExist:
-                donation = Donation.objects.create(amount=order[0].get('price'))
-                Order.objects.create(order_id=order[0].get('order'), position_id=order[0].get('positionid'), donation=donation)
-                return HttpResponseRedirect(reverse_lazy('donor_registration', kwargs={'pk': donation.pk}))
-        else:
-            return HttpResponseNotAllowed(permitted_methods='GET')
+        return process_order(kwargs['organizer'], kwargs['event'], kwargs['pseudo_id'])
 
 class PretixClaimedOrdersView(ListView):
     model = Order
